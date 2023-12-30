cd6e61fed10812f7157bb92be7ed543e4aa3a8a8
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2023-12-30 13:29:26 +0200

    marketting page updated

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,5 +1,12 @@
 import Image from "next/image"
 import Link from "next/link"
+import localFont from "next/font/local";
+
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({
+    src: "../public/fonts/font.woff2"
+});
 
 export const Logo = () => {
     return (
@@ -11,11 +18,14 @@ export const Logo = () => {
                 width={30} 
                 height={30}
                 />
-                <p className="text-lg text-neutral-700 pb-1">
+                <p className={cn(
+                "text-lg text-neutral-700 pb-1",
+                headingFont.className,
+                )}>
                     TaskGNix
                 </p>
             </div>
         </Link>
     );
-}
+};
     
