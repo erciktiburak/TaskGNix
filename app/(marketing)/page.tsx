1efdcb45088c9041444d8952801f2fc64c1c864f
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2023-12-31 08:44:51 +0200

    marketting page updated

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,29 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Medal } from "lucide-react";
-import { Poppins } from "next/font/google";
 import Link from "next/link";
-import localFont from "next/font/local";
 
-import { cn } from "@/lib/utils";
-
-const headingFont = localFont({
-    src: "../../public/fonts/font.woff2"
-});
-
-const textFont = Poppins({
-    subsets: ["latin"],
-    weight: [ "100","200","300","400","500","600","700","800","900"
-    ],
-    }
-);
 
 const MarketingPage = () => {
     return (
         <div className="flex item-center justify-center flex-col">
-            <div className="{cn(
-                flex items-center justify-center flex-col,
-                headingFont.className,
-                )}">
+            <div className="flex items-center justify-center flex-col">
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="w-6 h-6 mr-2" />
                     No 1 task management 
@@ -35,11 +18,11 @@ const MarketingPage = () => {
                     work forward.
                 </div>
             </div>
-            <div className={cn(
-                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto", textFont.className,
-            )}>
+            
+            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
                 TaskGNix Elevate teamwork, manage projects, boost productivity from high rises to home offices.
             </div>
+
               <Button className="mt-6" size="lg" asChild>
                 <Link href="/sign-up">
                     Get TaskGNix for free
