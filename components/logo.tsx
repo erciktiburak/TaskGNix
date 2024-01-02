c857f628a69539be35699f4709c283cde4313c3c
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-02 10:45:38 +0200

    Organizations Created

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,17 +1,26 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
+
+const headingFont = localFont({src: "../public/fonts/font.woff2",
+});
 
 export const Logo = () => {
     return (
         <Link href="/">
-            <div className="hover:opacity-75 transition items-center gap-x-2 hidden md-flex">
+            <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
                 <Image 
                 src="/logo.svg" 
                 alt="TaskGNix Logo"
                 width={30} 
                 height={30}
                 />
-                <p>
+                <p className={cn(
+                    "text-lg text-neutral-700 pb-1",
+                    headingFont.className,
+                    
+                    )}>
                     TaskGNix
                 </p>
             </div>
