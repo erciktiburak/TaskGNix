0807b7e7bcbc772c02655c005fa5b52c4e24bd27
Burak Ercikti <20180808082@ogr.akdeniz.edu.tr>
2024-01-04 09:57:06 +0200

    Sidebar Edited

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -10,6 +10,7 @@ import { Separator } from "@/components/ui/separator";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Accordion } from "@/components/ui/accordion";
 import { use } from "react";
+import { NavItem, Organization } from "./nav_item";
 
 interface SidebarProps {
     storageKey?: string;
@@ -20,9 +21,8 @@ export const Sidebar = ({
 }: SidebarProps) => {
     const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(
         storageKey, 
-        {}
-        
-        );
+        {}   
+    );
         const {
             organization: activeOrganization,
             isLoaded: isLoadedOrg,
@@ -44,11 +44,61 @@ export const Sidebar = ({
                 return acc;
             }, []);
 
-                
+            // { "123": true } => ["123"]
+
+        const onExpand = (id: string) => {
+            setExpanded((curr) => ({
+                ...curr,
+                [id]: !expanded[id],
+            }));
+        };
+        
+        if(!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+            return (
+                <>
+                <Skeleton />
+                </>    
+        );
+    };
 
     return (
-        <div>
-            Sidebar!
+        <>
+        <div className="font-medium text-xs flex items-center mb-1">
+            <span className="pl-4">
+                Workspaces!
+            </span>
+            <Button
+            asChild
+            type="button"
+            size="icon"
+            variant="ghost"
+            className="ml-auto"
+            
+            >
+                <Link href="/select-org">
+                    <Plus 
+                    className="h-4 w-4" 
+                    />
+                </Link>
+            </Button>
         </div>
+        <Accordion
+        type="multiple"
+        defaultValue={defaultAccordionValue}
+        className="space-y-2"
+        >
+            {userMemberships.data.map(({ organization }) => (
+                <NavItem 
+                    key={organization.id}
+                    isActive={activeOrganization?.id === organization.id}
+                    isExpanded={expanded[organization.id]}
+                    organization={organization as Organization}
+                    onExpand={onExpand}               
+                />
+
+            ))}
+            
+        </Accordion>
+        </>
     );
 };
